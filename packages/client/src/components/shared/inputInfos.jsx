9c3fcfb7a9f952dabff2dfbb637a89fe5d36ac08
mhammed-sed <m.seddaoui@esi-sba.dz>
2021-09-06 08:12:41 +0100

    task end textarea fixed

--- packages/client/src/components/shared/inputInfos.jsx
+++ packages/client/src/components/shared/inputInfos.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const InfoInput = (props) => {
+
     return(
         <div className="input__item d-flex align-items-flex-start">
             <span>{props.text}</span>
-            <input type="text" className="form-control" name={props.name} value={props.value}/>
+            <input 
+            type="text" 
+            className="form-control" 
+            name={props.name} 
+            placeholder={props.value}
+            />
         </div>
     );
 
